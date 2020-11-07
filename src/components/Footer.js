f8b7d90e5bb5621748c8f63858c98f7ecbe8e7f9
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-11-07 10:23:08 -0800

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className="footer text-center">
+            <span className="span-muted">&copy; copyright 2020 - elijah blaisdell</span>
+        </footer>
+    )
+}
