00d972f28747f9822f9ed713f59b84d2f686c75f
Elijah Blaisdell <elijahblaisdell@gmail.com>
2021-04-09 11:46:01 -0700

    Updated About

--- src/components/About.js
+++ src/components/About.js
@@ -14,10 +14,16 @@ export default function About() {
                     </figcaption>
                 </figure>
                 <p>
-                    I am a freelance classical singer turned web developer. As I singer I worked with some of the best professional choirs in the country, winning a Grammy Award with one of them in 2018. I've been studying software development for the past few years, hoping to eventually pivot into a new career that doesn't require me to be on the road so much. The loss of most of my singing work following the arrival of the pandemic allowed me to start working full time on making this career shift, recently culminating in the completion of the University of Washington's Full-Stack Web Development Bootcamp.
+                    I am a freelance classical singer turned web developer. As I singer I worked with some of the best professional 
+                    choirs in the country, winning a Grammy Award with one of them in 2018. I've been studying software development 
+                    for the past few years, hoping to eventually pivot into a new career that doesn't require me to be on the road so much. 
+                    The loss of most of my singing work following the arrival of the pandemic allowed me to start working full time on making 
+                    this career shift, and I've recently begun my new career with an internship at a young, fast-moving startup.
                     </p>
                 <p>
-                    In my work as both a musician and developer I consider myself to be primarily a craftsman. My job is to know my tools well, and strive for elegance in my use of those tools. I’m still near the beginning of my web-development journey and I have lots to learn, but so far I’ve shown myself to be passionate about my work, curious about everything, and effective at getting the job done.
+                    In my work as both a musician and developer I consider myself to be primarily a craftsman. My job is to know my tools well, 
+                    and strive for elegance in my use of those tools. I’m passionate about my work, curious about everything, and effective at 
+                    getting projects done on time and to a high standard.
                     </p>
                 <br></br>
                 <p>
